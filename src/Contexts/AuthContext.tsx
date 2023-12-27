dc20ab3d5d02e0f02e4abc128ce2ba00423d8a97
akihito7 <akihitopro7@gmail.com>
2023-12-27 00:13:49 -0300

    adicionado função para logar

--- src/Contexts/AuthContext.tsx
+++ src/Contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, ReactNode } from "react";
+import { createContext, useContext, ReactNode, useState } from "react";
 import { api } from "../axios";
 
 
@@ -9,23 +9,52 @@ interface AuthContextProviderProps {
 }
 
 type AuthContextProps = {
-    signup : (UserInfo : SignupProps) => void;
+    signln: (credetials: SignlnProps) => void;
+    signup: (UserInfo: SignupProps) => void;
+}
+
+type SignlnProps = {
+    email: string;
+    password: string
 }
 
 type SignupProps = {
-    email : string;
-    name : string;
-    password : string;
+    email: string;
+    name: string;
+    password: string;
 }
 
 function AuthContextProvider({ children }: AuthContextProviderProps) {
 
-    async function signup(userInfo : SignupProps){
+    const [user, setUser] = useState(null);
+    const [token, setToken] = useState(null)
+
+    async function signln(credentials: SignlnProps) {
+
+        try {
+            const response = await api.post("/auth/siglnl", {
+                email: credentials.email,
+                password: credentials.password
+            });
+
+            console.log(user, token)
+
+            setUser(response.data.user);
+            setToken(response.data.token);
+
+
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
+    async function signup(userInfo: SignupProps) {
         try {
             await api.post("/auth/signup", {
-                email : userInfo.email,
-                name : userInfo.name,
-                password : userInfo.password
+                email: userInfo.email,
+                name: userInfo.name,
+                password: userInfo.password
             })
 
         } catch (error) {
@@ -34,7 +63,7 @@ function AuthContextProvider({ children }: AuthContextProviderProps) {
     }
 
     return (
-        <AuthContext.Provider value={{signup}}>
+        <AuthContext.Provider value={{ signup, signln }}>
             {children}
         </AuthContext.Provider>
     );
