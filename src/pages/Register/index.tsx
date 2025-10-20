6cda91ef386fd99d97ed33fb1c15378bbc6e1a54
akihito7 <akihitopro7@gmail.com>
2025-10-20 12:50:19 -0300

    refactor: refatorando codigo antigos aplicando conhecimento novo, e focando em aplicar o SOLID

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from "react";
 
 import {
   Container,
@@ -15,66 +15,94 @@ import {
   ButtonSend,
   Header,
   ExtendedArea,
-} from './style'
+} from "./style";
 
-import { Input } from '../../components/Input'
-import { useNavigate } from "react-router-dom"
+import { Input } from "../../components/Input";
+import { useNavigate } from "react-router-dom";
 
-import { categories } from '../../utils/categories';
+import { categories } from "../../utils/categories";
 
-import { Controller, useForm } from "react-hook-form"
-import { api } from '../../axios';
-import { useAuth } from '../../Contexts/AuthContext';
-import { Menu } from '../../components/Menu';
+import { Controller, useForm } from "react-hook-form";
+import { api } from "../../axios";
+import { useAuth } from "../../Contexts/AuthContext";
+import { Menu } from "../../components/Menu";
+import { format } from "date-fns";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
-type PropsTransaction = {
-  name: string,
-  price: string,
-}
+const TransactionSchema = yup.object({
+  name: yup.string().required(),
+  price: yup.number().required(),
+  category: yup.string().required(),
+  typeTransaction: yup.string().required(),
+});
+
+type TransactionSchemaProps = yup.InferType<typeof TransactionSchema>;
+
 export function Register() {
-
   const [categoryOpen, setCategoryOpen] = useState(false);
-  const [typeTransaction, setTypeTransaction] = useState('');
-  const [categorySelected, setCategorySelected] = useState('');
   const navigate = useNavigate();
 
-  const { control, handleSubmit, reset } = useForm();
-  const { user, logout } = useAuth();
+  const {
+    control,
+    handleSubmit,
+    reset,
+    formState: { errors },
+    setValue,
+    watch,
+  } = useForm({
+    resolver: yupResolver(TransactionSchema),
+  });
+
+  const typeTransaction = watch("typeTransaction");
+  const categorySelected = watch("category");
+
+  const { user } = useAuth();
 
   function handleCategoryOpen() {
-    if (categoryOpen) setCategoryOpen(false)
-    else setCategoryOpen(true)
-  };
+    if (categoryOpen) setCategoryOpen(false);
+    else setCategoryOpen(true);
+  }
 
-  async function handleRegisterTransaction(transaction: PropsTransaction) {
+  async function createTransaction(
+    transaction: TransactionSchemaProps,
+    date: string
+  ) {
+    await api.post("/transaction/", {
+      name: transaction.name,
+      value: transaction.price,
+      category: categorySelected,
+      type: typeTransaction,
+      date,
+      user_id: user.id,
+    });
+  }
 
-    const today = new Date();
-    const day = String(today.getDate()).padStart(2, "0");
-    let month = String(today.getMonth() + 1).padStart(2, "0");
-    let year = today.getFullYear();
+  function resetFormFields() {
+    reset({
+      name: "",
+      price: 0,
+      category: "",
+      typeTransaction: "",
+    });
+  }
 
-    if(!Number(transaction.price)) return alert("Forneça um valor válido!")
-
+  async function handleRegisterTransaction(
+    transaction: TransactionSchemaProps
+  ) {
+    const dateFormatted = format(new Date(), "dd/MM/yyyy");
     try {
-      await api.post("/transaction/", {
-        name: transaction.name,
-        value: transaction.price,
-        category: categorySelected,
-        type: typeTransaction,
-        date: `${day}/${month}/${year}`,
-        user_id: user.id,
-      });
-      reset({
-        'name': '',
-        'price': '',
-      });
-      setTypeTransaction('');
-      setCategorySelected('');
-      navigate('/');
+      await createTransaction(transaction, dateFormatted);
+      resetFormFields();
+      navigate("/");
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
   }
+
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
 
   return (
     <Container>
@@ -84,13 +112,12 @@ export function Register() {
             <Title>Cadastro</Title>
 
             <Controller
-              name='name'
+              name="name"
               control={control}
               render={({ field: { onChange, value } }) => (
-
                 <Input
-                  errorMessage={null}
-                  placeholder='Nome'
+                  errorMessage={errors.name?.message ?? null}
+                  placeholder="Nome"
                   onChange={(event) => onChange(event.target.value)}
                   value={value}
                 />
@@ -98,13 +125,13 @@ export function Register() {
             />
 
             <Controller
-              name='price'
+              name="price"
               control={control}
               render={({ field: { onChange, value } }) => (
-
                 <Input
-                  errorMessage={null}
-                  placeholder='Preço'
+                  type="number"
+                  errorMessage={errors.price?.message ?? null}
+                  placeholder="Preço"
                   onChange={(event) => onChange(event.target.value)}
                   value={value}
                 />
@@ -114,69 +141,59 @@ export function Register() {
             <ContainerButtons>
               <ButtonTypeTransaction
                 selected={typeTransaction === "income" ? true : false}
-                color='green'
-                onClick={() => { setTypeTransaction("income") }}
+                color="green"
+                onClick={() => {
+                  setValue("typeTransaction", "income");
+                }}
               >
-                <ButtonIcon src='Entradas.svg' />
+                <ButtonIcon src="Entradas.svg" />
                 Income
               </ButtonTypeTransaction>
 
               <ButtonTypeTransaction
                 selected={typeTransaction === "outcome" ? true : false}
-                color='red'
-                onClick={() => { setTypeTransaction("outcome") }}
+                color="red"
+                onClick={() => {
+                  setValue("typeTransaction", "outcome");
+                }}
               >
-                <ButtonIcon src='Saidas.svg' />
+                <ButtonIcon src="Saidas.svg" />
                 Outcome
               </ButtonTypeTransaction>
-
             </ContainerButtons>
 
             <ContainerCategory>
               <ButtonSelect onClick={handleCategoryOpen}>
-                {categorySelected != '' ? categorySelected : "Categorias"}
+                {categorySelected != "" ? categorySelected : "Categorias"}
               </ButtonSelect>
               <ContainerButtonsCategory
-                className={categoryOpen ? '' : 'hidden'}
+                className={categoryOpen ? "" : "hidden"}
               >
-
-                {
-                  categories.map((category) => (
-                    <ButtonOfCategory
-                      onClick={() => {
-                        setCategorySelected(category.categoria)
-                        setCategoryOpen(false)
-                      }}
-                    >
-                      <IconCategory
-                        src={category.icon}
-                        alt='icone da categoria'
-                      />
-                      {category.categoria}
-                    </ButtonOfCategory>
-                  ))
-                }
-
+                {categories.map((category) => (
+                  <ButtonOfCategory
+                    onClick={() => {
+                      setValue("category", category.categoria);
+                      setCategoryOpen(false);
+                    }}
+                  >
+                    <IconCategory
+                      src={category.icon}
+                      alt="icone da categoria"
+                    />
+                    {category.categoria}
+                  </ButtonOfCategory>
+                ))}
               </ContainerButtonsCategory>
             </ContainerCategory>
 
-            <ButtonSend
-              onClick={handleSubmit(handleRegisterTransaction)}
-            >
+            <ButtonSend onClick={handleSubmit(handleRegisterTransaction)}>
               Salvar
             </ButtonSend>
-
-
           </ContainerForm>
-
         </ExtendedArea>
       </Header>
-      {
-
-
-      }
-      <Menu screenSelected='register' />
-
+      {}
+      <Menu screenSelected="register" />
     </Container>
-  )
+  );
 }
